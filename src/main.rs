0105074ff4ceea244f51b84ebb90bb17709ed644
Paolo D'Incau <paolo.dincau@gmail.com>
2023-03-23 18:25:47 +0100

    make test pass

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ struct TriggerUnusualSpendingEmail {
 }
 
 impl TriggerUnusualSpendingEmail {
-    pub fn trigger(&mut self, p0: &str) {
-        self.payments.by_user_and_month();
+    pub fn trigger(&mut self, user_id: &str) {
+        self.payments.by_user_and_period(user_id, 2023, 3);
     }
 }
 
 #[automock]
 trait Payments {
-    fn by_user_and_month(&mut self);
+    fn by_user_and_period(&mut self, user_id: &str, year: u32, month: u8);
 }
 
 #[cfg(test)]
@@ -26,17 +26,19 @@ mod test {
 
     #[test]
     fn it_retrieves_payments_for_the_current_month() {
+        let a_user_id = "aUserId";
+
         let mut payments = MockPayments::new();
 
         payments
-            .expect_by_user_and_month()
+            .expect_by_user_and_period()
             .times(1)
-            .returning(|| ());
+            .returning(|_, _, _| ());
 
         let mut application = TriggerUnusualSpendingEmail {
             payments: Box::new(payments),
         };
 
-        application.trigger("aUserId");
+        application.trigger(a_user_id);
     }
 }
